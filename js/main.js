d4aedb1c8a3a275ce7f554d7ae9b6b6d83867fa2
aureabeltran <aureabeltranmanzo@gmail.com>
2016-07-20 09:25:04 -0400

    arreglo hover

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 var text = $("#responder");
 var chatPanel = $(".chat-panel");
-
-/*var newHour = new Date();
+var newHour = new Date();
 	newHour.setTime(newHour.getTime());
 	var hora = new Date().getHours();
 	if (hora < 10) {
@@ -11,13 +10,12 @@ var chatPanel = $(".chat-panel");
 	if (minuto < 10) {
 		minuto = "0" + minuto;
 	}
-	$("#hora").html(hora + ":" + minuto);
-	*/
 
 $(text).keypress(function( event ) {
   if ( event.which == 13 ) {
-     	chatPanel.append('<div class="chat"></div>');
-	 	$(".chat").append("<p class='texto'>" + text.val() + "</p>");
+     	chatPanel.append('<div>' + "<p class='texto'>" + text.val() + "</p>" + "<h5 class='texto'>" + hora + ":" + minuto + "</h5>" + '</div>');
 	 	text.val("");	
     }
+    var chat  = $(".texto").parent();
+    chat.addClass("chat");
 });
